093ac3319b01a714270a769ddd24a54f656012e3
Gabe Shackle <gabe.shackle@gmail.com>
2014-10-17 15:14:42 -0500

    Initial blank walker commit.

--- Basic Unordered List/basic-unordered-list-nw.php
+++ Basic Unordered List/basic-unordered-list-nw.php
@@ -10,6 +10,105 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-class Basic_Unordered_List extends Walker_Nav_Menu {
+class Walker_Basic_Unordered_List extends Walker_Page {
 
-}
+	/**
+	 * @see Walker::start_lvl()
+	 * @since 2.1.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int $depth Depth of page. Used for padding.
+	 * @param array $args
+	 */
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "\n$indent<ul>\n";
+	}
+
+	/**
+	 * @see Walker::start_el()
+	 * @since 2.1.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $page Page data object.
+	 * @param int $depth Depth of page. Used for padding.
+	 * @param int $current_page Page ID.
+	 * @param array $args
+	 */
+	public function start_el( &$output, $page, $depth = 0, $args = array(), $current_page = 0 ) {
+		if ( $depth ) {
+			$indent = str_repeat( "\t", $depth );
+		} else {
+			$indent = '';
+		}
+
+		$active_item = false;
+
+		if ( ! empty( $current_page ) ) {
+			$_current_page = get_post( $current_page );
+			if ( in_array( $page->ID, $_current_page->ancestors ) ) {
+				$active_item = true;
+			}
+			if ( $page->ID == $current_page ) {
+				$active_item = true;
+			} elseif ( $_current_page && $page->ID == $_current_page->post_parent ) {
+				$active_item = true;
+			}
+		} elseif ( $page->ID == get_option('page_for_posts') ) {
+			$active_item = true;
+		}
+
+		if ( true === $active_item ) {
+			$css_class[] = 'active';
+		}
+
+		/**
+		 * Filter the list of CSS classes to include with each page item in the list.
+		 *
+		 * @since 2.8.0
+		 *
+		 * @see wp_list_pages()
+		 *
+		 * @param array   $css_class    An array of CSS classes to be applied
+		 *                             to each list item.
+		 * @param WP_Post $page         Page data object.
+		 * @param int     $depth        Depth of page, used for padding.
+		 * @param array   $args         An array of arguments.
+		 * @param int     $current_page ID of the current page.
+		 */
+		$css_classes = implode( ' ', apply_filters( 'page_css_class', $css_class, $page, $depth, $args, $current_page ) );
+
+		if ( '' === $page->post_title ) {
+			$page->post_title = sprintf( __( '#%d (no title)' ), $page->ID );
+		}
+
+		$args['link_before'] = empty( $args['link_before'] ) ? '' : $args['link_before'];
+		$args['link_after'] = empty( $args['link_after'] ) ? '' : $args['link_after'];
+
+		if ( ! empty( $css_classes ) ) {
+			$css_class_output = ' class="' . $css_classes . '"';
+		}
+
+		/** This filter is documented in wp-includes/post-template.php */
+		$output .= $indent . sprintf(
+				'<li%s><a href="%s">%s%s%s</a>',
+				$css_class_output,
+				get_permalink( $page->ID ),
+				$args['link_before'],
+				apply_filters( 'the_title', $page->post_title, $page->ID ),
+				$args['link_after']
+			);
+
+		if ( ! empty( $args['show_date'] ) ) {
+			if ( 'modified' == $args['show_date'] ) {
+				$time = $page->post_modified;
+			} else {
+				$time = $page->post_date;
+			}
+
+			$date_format = empty( $args['date_format'] ) ? '' : $args['date_format'];
+			$output .= " " . mysql2date( $date_format, $time );
+		}
+	}
+
+} // Walker_Basic_Unordered_List
